98cedab0b83307a5b0f2647619a79e6be253da39
ScriptSaga03 <mehtab185198131@gmail.com>
2025-03-16 13:34:01 +0530

    url changed

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.set("trust proxy", 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["https://authentication-app-9ywt.onrender.com", "http://localhost:5173"],
+  origin: ["https://authentication-app-9ywt.onrender.com/"],
   credentials: true
 }));
 
